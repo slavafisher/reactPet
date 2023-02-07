a0f5e981fb41b464eaad780833f2acd121251588
Yaroslav Rybak <randomnuy@gmail.com>
2023-02-07 12:16:39 -0600

    pra

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+ const numberOfFilms = prompt(`Скільки фільмів ви вже переглянули?`, '');
+
+ const lastSeingFilm = prompt(`Один з останніх переглянутих фільмів?`, '');
+ const filmGrade = prompt(`На скільки ви оціните його?`, '');
+ console.log(lastSeingFilm);
+
+ const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+    },
+    actors: {},
+    genres: [],
+    privat: false
+ };
+
+
+ 
+ personalMovieDB.movies[lastSeingFilm] = filmGrade;
+ console.log(personalMovieDB);
